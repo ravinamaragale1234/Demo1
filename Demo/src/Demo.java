1c692dd36e813a2ed5daaf75ebeb9e2b769fe1c9
ravinamaragale1234 <37179990+ravinamaragale1234@users.noreply.github.com>
2018-03-13 14:49:31 +0530

    Update Demo.java

--- Demo/src/Demo.java
+++ Demo/src/Demo.java
@@ -3,9 +3,11 @@ public class Demo
 { 
 	public static void main(String[] args)
 	{
-		int a,b,c;
+		int a=5;
+		int b=6;
+		int c;
 		c=a+b;
-		System.out.println("c");
+		System.out.println(c);
 		System.out.println("Hello World");
 	}
 
